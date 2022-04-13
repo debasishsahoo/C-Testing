6092f57f1baa8508aaa8c76e4d3f92572f423a6f
debasishsahoo <debasish.sahoo96@gmail.com>
2022-04-13 11:32:45 +0530

    BYID

---
+++ TestingIntroductions/3.FindElement/Program.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3.FindElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ByID
+            Console.Write("Test Case Started:");
+            string name = "Debasish Sahoo";
+            string email = "debasish.sahoo96@gmail.com";
+            string Cadd = "Kolkata";
+            string Padd = "Bhowanipur";
+
+            IWebDriver Driver = new ChromeDriver();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+
+            Thread.Sleep(2000);
+
+            Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+
+            Thread.Sleep(2000);
+            Driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+            Driver.FindElement(By.Id("userName")).SendKeys(name);
+            Thread.Sleep(2000);
+            Driver.FindElement(By.Id("userEmail")).SendKeys(email);
+            Thread.Sleep(2000);
+            Driver.FindElement(By.Id("currentAddress")).SendKeys(Cadd);
+            Thread.Sleep(2000);
+            Driver.FindElement(By.Id("permanentAddress")).SendKeys(Padd);
+            Thread.Sleep(2000);
+
+            js.ExecuteScript("window.scrollBy(0,500)");
+            Thread.Sleep(2000);
+
+            Driver.FindElement(By.Id("submit")).Click();
+
+            Task.Delay(120000).Wait();
+
+            Console.Write("Test Case Closed:");
+            Driver.Close();
+            Driver.Quit(); 
+            #endregion
+
+        }
+    }
+}
