f789da778d0e3b23dc949119145cb3aa2f13933e
debasishsahoo <debasish.sahoo96@gmail.com>
2022-04-20 12:14:49 +0530

    xpath

--- TestingIntroductions/3.FindElement/Program.cs
+++ TestingIntroductions/3.FindElement/Program.cs
@@ -14,44 +14,42 @@ namespace _3.FindElement
         static void Main(string[] args)
         {
             #region ByID
-            //Console.Write("Test Case Started:");
-            //string name = "Debasish Sahoo";
-            //string email = "debasish.sahoo96@gmail.com";
-            //string Cadd = "Kolkata";
-            //string Padd = "Bhowanipur";
+//Console.Write("Test Case Started:");
+//string name = "Debasish Sahoo";
+//string email = "debasish.sahoo96@gmail.com";
+//string Cadd = "Kolkata";
+//string Padd = "Bhowanipur";
 
-            //IWebDriver Driver = new ChromeDriver();
-            //IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+//IWebDriver Driver = new ChromeDriver();
+//IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
 
-            //Thread.Sleep(2000);
+//Thread.Sleep(2000);
 
-            //Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+//Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
 
-            //Thread.Sleep(2000);
-            //Driver.Manage().Window.Maximize();
-            //Thread.Sleep(2000);
-            //Driver.FindElement(By.Id("userName")).SendKeys(name);
-            //Thread.Sleep(2000);
-            //Driver.FindElement(By.Id("userEmail")).SendKeys(email);
-            //Thread.Sleep(2000);
-            //Driver.FindElement(By.Id("currentAddress")).SendKeys(Cadd);
-            //Thread.Sleep(2000);
-            //Driver.FindElement(By.Id("permanentAddress")).SendKeys(Padd);
-            //Thread.Sleep(2000);
+//Thread.Sleep(2000);
+//Driver.Manage().Window.Maximize();
+//Thread.Sleep(2000);
+//Driver.FindElement(By.Id("userName")).SendKeys(name);
+//Thread.Sleep(2000);
+//Driver.FindElement(By.Id("userEmail")).SendKeys(email);
+//Thread.Sleep(2000);
+//Driver.FindElement(By.Id("currentAddress")).SendKeys(Cadd);
+//Thread.Sleep(2000);
+//Driver.FindElement(By.Id("permanentAddress")).SendKeys(Padd);
+//Thread.Sleep(2000);
 
-            //js.ExecuteScript("window.scrollBy(0,500)");
-            //Thread.Sleep(2000);
+//js.ExecuteScript("window.scrollBy(0,500)");
+//Thread.Sleep(2000);
 
-            //Driver.FindElement(By.Id("submit")).Click();
+//Driver.FindElement(By.Id("submit")).Click();
 
-            //Task.Delay(120000).Wait();
+//Task.Delay(120000).Wait();
 
-            //Console.Write("Test Case Closed:");
-            //Driver.Close();
-            //Driver.Quit(); 
+//Console.Write("Test Case Closed:");
+//Driver.Close();
+//Driver.Quit(); 
             #endregion
-
-
 
         }
     }
---
+++ TestingIntroductions/4.xpathintro/Program.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4.xpathintro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver Driver = new ChromeDriver();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            Thread.Sleep(2000);
+
+            Driver.Navigate().GoToUrl("https://phptravels.com/demo");
+            Thread.Sleep(2000);
+
+            Driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+
+            //Driver.FindElement(By.XPath("//a[@href='//www.phptravels.net/admin']")).Click();
+
+            Driver.FindElement(By.XPath("//a[contains(@href,'admin')]")).Click();
+            Thread.Sleep(2000);
+            //string currentwindows = Driver.WindowHandles;
+
+            string p = Driver.WindowHandles[0];
+            string c = Driver.WindowHandles[1];
+            Driver.SwitchTo().Window(c);
+
+            Thread.Sleep(2000);
+            Driver.Manage().Window.Maximize();
+
+            Thread.Sleep(2000);
+            Driver.FindElement(By.XPath("//input[@name='email' and @type='text']")).SendKeys("admin@phptravels.com");
+
+            Thread.Sleep(1000);
+            Driver.FindElement(By.XPath("//input[@name='password' and @type='password']")).SendKeys("demoadmin");
+
+            Thread.Sleep(1000);
+            Driver.FindElement(By.XPath("//span[text()='Login']")).Click();
+
+            Thread.Sleep(3000);
+            Driver.Close();
+            Driver.Quit();
+            
+
+
+
+        }
+    }
+}
